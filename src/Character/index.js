6f2a0eee680f6902f6573197d7717acecc5e58fe
Tyler Cromwell <tjc6185@gmail.com>
2019-10-24 22:16:32 -0400

    UI: Centering character creation component on screen

--- src/Character/index.js
+++ src/Character/index.js
@@ -1,7 +1,9 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { races } from './Races'
 import { classes } from './Classes'
+
+import './index.css'
 
 
 function CreateCharacter(props) {
@@ -11,31 +13,42 @@ function CreateCharacter(props) {
 
 
   useEffect(() => {
-    let selectClass = document.getElementById("selectClass");
-    let selectedText = selectClass.options[selectClass.selectedIndex].text;
+    let characterInputClass = document.getElementById("characterInputClass");
+    let selectedText = characterInputClass.options[characterInputClass.selectedIndex].text;
     setClass(selectedText)
   }, [race])
 
 
   return (
-    <Fragment>
-      <span className="label">Name:</span>
-      <input
-        id="inputName"
-        type="text"
-        maxLength="14"
-        onChange={evt => setName(evt.target.value)}
-      />
-      <select id="selectRace" onClick={evt => setRace(evt.target.value)}>
-        {races.map(r => <option key={r} value={r}>{r}</option>)}
-      </select>
-      <select id="selectClass" onClick={evt => setClass(evt.target.value)}>
-        {classes[race].map(c => <option key={c} value={c}>{c}</option>)}
-      </select>
-      <button onClick={() => props.finish(name, race, class_)}>
-        Create!
-      </button>
-    </Fragment>
+    <div id="characterInputContainer">
+      <div id="characterInputContent">
+        <input
+          id="characterInputName"
+          type="text"
+          maxLength="14"
+          onChange={evt => setName(evt.target.value)}
+          placeholder="Enter a name"
+        />
+        <select
+          id="characterInputRace"
+          onClick={evt => setRace(evt.target.value)}
+        >
+          {races.map(r => <option key={r} value={r}>{r}</option>)}
+        </select>
+        <select
+          id="characterInputClass"
+          onClick={evt => setClass(evt.target.value)}
+        >
+          {classes[race].map(c => <option key={c} value={c}>{c}</option>)}
+        </select>
+        <button
+          id="characterInputCreateButton"
+          onClick={() => props.finish(name, race, class_)}
+        >
+          Create!
+        </button>
+      </div>
+    </div>
   )
 }
 
