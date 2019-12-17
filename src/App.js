135ed7703ffdbb8177c3a74fae24308dad66b967
Tyler Cromwell <tjc6185@gmail.com>
2019-12-16 20:35:31 -0500

    UI: Added placeholders for other componenets

--- src/App.js
+++ src/App.js
@@ -259,6 +259,7 @@ function App() {
       />
   );
 
+
   return <div className="App">
     {content}
   </div>
--- src/HUD/index.js
+++ src/HUD/index.js
@@ -1,15 +1,25 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 import ProgressBar from '../ProgressBar'
 
+import './index.css'
+
 
 function HUD(props) {
-  return <Fragment>
-    <div><span className="label">{props.character.name}, {props.character.race} {props.character.class_} (Level {props.character.level})</span></div>
-    <div><ProgressBar id='Health' value={props.character.health} max={props.character.maxHealth}/></div>
-    <div><ProgressBar id='Stamina' value={props.character.stamina} max={props.character.maxStamina}/></div>
-    <div><ProgressBar id='Mana' value={props.character.mana} max={props.character.maxMana}/></div>
-  </Fragment>
+  return <div id="hudContainer">
+    <div id="hudStats">
+      <div><span className="label">{props.character.name}, {props.character.race} {props.character.class_} ({props.character.level})</span></div>
+      <div><ProgressBar id='Health' value={props.character.health} max={props.character.maxHealth}/></div>
+      <div><ProgressBar id='Stamina' value={props.character.stamina} max={props.character.maxStamina}/></div>
+      <div><ProgressBar id='Mana' value={props.character.mana} max={props.character.maxMana}/></div>
+    </div>
+    <div id="hudBackpack">
+      <span className="label">Backpack goes here</span>
+    </div>
+    <div id="hudSpellbook">
+      <span className="label">Spellbook goes here</span>
+    </div>
+  </div>
 }
 
 
